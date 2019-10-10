13695c72f2a5ab0c9ead75a7cc5609e47fa33c5f
Raja Kondla <rajakondla@gmail.com>
2019-10-10 13:08:27 +0530

    first commit

---
+++ Tweetbook/Controllers/V1/PostsController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Tweetbook.Cache;
+using Tweetbook.Contracts.V1;
+using Tweetbook.Contracts.V1.Requests;
+using Tweetbook.Contracts.V1.Responses;
+using Tweetbook.Domain;
+using Tweetbook.Extensions;
+using Tweetbook.Services;
+
+namespace Tweetbook.Controllers.V1
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class PostsController : Controller
+    {
+        private readonly IPostService _postService;
+        private readonly IMapper _mapper;
+
+        public PostsController(IPostService postService, IMapper mapper)
+        {
+            _postService = postService;
+            _mapper = mapper;
+        }
+
+        [HttpGet(ApiRoutes.Posts.GetAll)]
+        [Cached(600)]
+        public async Task<IActionResult> GetAll()
+        {
+            var posts = await _postService.GetPostsAsync();
+            return Ok(_mapper.Map<List<PostResponse>>(posts));
+        }
+
+        [HttpPut(ApiRoutes.Posts.Update)]
+        public async Task<IActionResult> Update([FromRoute]Guid postId, [FromBody] UpdatePostRequest request)
+        {
+            var userOwnsPost = await _postService.UserOwnsPostAsync(postId, HttpContext.GetUserId());
+
+            if (!userOwnsPost)
+            {
+                return BadRequest(new ErrorResponse(new ErrorModel{Message = "You do not own this post"}));
+            }
+
+            var post = await _postService.GetPostByIdAsync(postId);
+            post.Name = request.Name;
+
+            var updated = await _postService.UpdatePostAsync(post);
+
+            if(updated)
+                return Ok(_mapper.Map<PostResponse>(post));
+
+            return NotFound();
+        }
+
+        [HttpDelete(ApiRoutes.Posts.Delete)]
+        public async Task<IActionResult> Delete([FromRoute] Guid postId)
+        {
+            var userOwnsPost = await _postService.UserOwnsPostAsync(postId, HttpContext.GetUserId());
+
+            if (!userOwnsPost)
+            {
+                return BadRequest(new ErrorResponse(new ErrorModel{Message = "You do not own this post"}));
+            }
+            
+            var deleted = await _postService.DeletePostAsync(postId);
+
+            if (deleted)
+                return NoContent();
+
+            return NotFound();
+        }
+
+        [HttpGet(ApiRoutes.Posts.Get)]
+        [Cached(600)]
+        public async Task<IActionResult> Get([FromRoute]Guid postId)
+        {
+            var post = await _postService.GetPostByIdAsync(postId);
+
+            if (post == null)
+                return NotFound();
+
+            return Ok(_mapper.Map<PostResponse>(post));
+        }
+
+        [HttpPost(ApiRoutes.Posts.Create)]
+        public async Task<IActionResult> Create([FromBody] CreatePostRequest postRequest)
+        {
+            var newPostId = Guid.NewGuid();
+            var post = new Post
+            {
+                Id = newPostId,
+                Name = postRequest.Name,
+                UserId = HttpContext.GetUserId(),
+                Tags = postRequest.Tags.Select(x=> new PostTag{PostId = newPostId, TagName = x}).ToList()
+            };
+            
+            await _postService.CreatePostAsync(post);
+
+            var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.ToUriComponent()}";
+            var locationUri = baseUrl + "/" + ApiRoutes.Posts.Get.Replace("{postId}", post.Id.ToString());
+            
+            return Created(locationUri, _mapper.Map<PostResponse>(post));
+        }
+    }
+}
---
+++ Tweetbook/Startup.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Tweetbook.Installers;
+using SwaggerOptions = Tweetbook.Options.SwaggerOptions;
+
+namespace Tweetbook
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.InstallServicesInAssembly(Configuration);
+            services.AddAutoMapper(typeof(Startup));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IMapper mapper)
+        {
+            mapper.ConfigurationProvider.AssertConfigurationIsValid();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+            
+            var swaggerOptions = new SwaggerOptions();
+            Configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
+
+            app.UseSwagger(option => { option.RouteTemplate = swaggerOptions.JsonRoute; });
+
+            app.UseSwaggerUI(option =>
+            {
+                option.SwaggerEndpoint(swaggerOptions.UiEndpoint, swaggerOptions.Description);
+            });
+
+            app.UseMvc();
+        }
+    }
+}
